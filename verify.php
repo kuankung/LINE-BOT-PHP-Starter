4eca661217feab7c5c2128ea32425da030cbb138
kuankung <kuankung.s@gmail.com>
2018-02-15 18:45:27 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'Uad6a6ac7d37ece4a8875cc2cf65a5a6f
+$access_token = 'bNlMZF/qGOc5bxNA2+dbh9VrTe/AHoXcszXOgpHUrjkTBv7IrjlzxDk9updVmNCF4mXB6oXm23inlEvJeISVLy1Xmla0zBvk0Pcw6U0MJOLG1b2x3e1y5j+PH7PcVvkyZplQTqV5agy7Mybn6bfsOQdB04t89/1O/w1cDnyilFU=
 ';
 
 $url = 'https://api.line.me/v1/oauth/verify';
